ed854137eec434a060cacaa1c2bc49ee1ae4259e
s.k.noe@hinoshiba.com <s.k.noe@hinoshiba.com>
2020-04-05 16:22:37 +0900

    added testcode

--- src/onamaeddns/exec/onamaeddns/onamaeddns.go
+++ src/onamaeddns/exec/onamaeddns/onamaeddns.go
@@ -1,13 +1,51 @@
 package main
 
 import (
+	"regexp"
 	"log"
-	"onamaeddns"
+	"time"
+	"crypto/tls"
+	"github.com/google/goexpect"
+)
+
+var (
+	timeout = 10 * time.Minute
+)
+
+const (
+	TEST_SERVER = "www.i.hinoshiba.com:443"
 )
 
 func main() {
+	conn, err := tls.Dial("tcp", TEST_SERVER, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
 
-	var t onamaeddns.Te
-	log.Println(t)
-	return
+	resCh := make(chan error)
+
+	exp, _, err := expect.SpawnGeneric(&expect.GenOptions{
+		In:  conn,
+		Out: conn,
+		Wait: func() error {
+			return <-resCh
+		},
+		Close: func() error {
+			close(resCh)
+			return conn.Close()
+		},
+		Check: func() bool { return true },
+	}, timeout, expect.Verbose(true))
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer exp.Close()
+
+	promptRE := regexp.MustCompile("HTTP")
+	exp.Send("" + "\n")
+	exp.Send("" + "\n")
+	exp.Send("." + "\n")
+	log.Println(exp.Expect(promptRE, timeout))
 }
--- xcompile.sh
+++ xcompile.sh
@@ -4,7 +4,7 @@ export GOPATH="`pwd`"
 ls -1 src/${work} | while read row ; do
   echo "compile ${row}"
   GOOS=linux GOARCH=amd64 go install ${work}/${row}
-  GOOS=windows GOARCH=amd64 go install ${work}/${row}
+#  GOOS=windows GOARCH=amd64 go install ${work}/${row}
   GOOS=darwin GOARCH=amd64 go install ${work}/${row}
 done
 echo "done"
