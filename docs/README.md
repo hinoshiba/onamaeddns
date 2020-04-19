fa37b9524ea694ba82e98cc2847112225ced4b98
s.k.noe@hinoshiba.com <s.k.noe@hinoshiba.com>
2020-04-19 23:28:02 +0900

    updated command and added docments

--- docs/README.md
+++ docs/README.md
@@ -4,3 +4,46 @@ go-onamaeddns
 * [onamae.com](https://help.onamae.com/answer/7920) ddns client.
 	* like a [this client](https://help.onamae.com/answer/7920).
 
+## usage
+
+```
+[user@host]$ onamaeddns [-c <path of credential>] <host> <domain> <ip address>
+Usage of bin/onamaeddns:
+  -c string
+        path of credential. default "~/.onamaeddns"
+```
+
+## as a package
+
+```
+package main
+
+import (
+	"log"
+	"time"
+)
+
+import (
+	"github.com/hinoshiba/go-onamaeddns/src/onamaeddns"
+)
+
+func main() {
+	cl, err := onamaeddns.Dial("ddnsclient.onamae.com:65010", "username", "password", time.Minute)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer cl.Close()
+
+	if err := cl.UpdateIPv4("mytest", "example.com", "127.0.0.1"); err != nil {
+		log.Println(err)
+		return
+	}
+	log.Println("updated")
+}
+```
+
+
+## References
+
+* [LinuxやMacで お名前.com ダイナミックDNS の IPアドレスを更新する https://qiita.com/ats124/items/59ec0f444d00bbcea27d](https://qiita.com/ats124/items/59ec0f444d00bbcea27d)
--- src/onamaeddns/exec/onamaeddns/onamaeddns.go
+++ src/onamaeddns/exec/onamaeddns/onamaeddns.go
@@ -1,8 +1,13 @@
 package main
 
 import (
-	"log"
+	"os"
+	"os/user"
+	"fmt"
+	"flag"
 	"time"
+	"bufio"
+	"strings"
 )
 
 import (
@@ -10,24 +15,103 @@ import (
 )
 
 var (
-	timeout = 10 * time.Minute
+	timeout = 10 * time.Second
+
+	Pass      string
+	User      string
+	IpAddress string
+	Host      string
+	Domain    string
 )
 
 const (
 	ONAMAE_SV = "ddnsclient.onamae.com:65010"
 )
 
-func main() {
-	cl, err := onamaeddns.Dial(ONAMAE_SV, "username", "password", timeout)
+func ddns() error {
+	cl, err := onamaeddns.Dial(ONAMAE_SV, User, Pass, timeout)
 	if err != nil {
-		log.Println(err)
-		return
+		return err
 	}
 	defer cl.Close()
 
-	if err := cl.UpdateIPv4("mytest", "example.com", "127.0.0.1"); err != nil {
-		log.Println(err)
-		return
+	if err := cl.UpdateIPv4(Host, Domain, IpAddress); err != nil {
+		return err
 	}
-	log.Println("updated")
+	return nil
+}
+
+func die(s string, msg ...interface{}) {
+	fmt.Fprintf(os.Stderr, s + "\n" , msg...)
+	os.Exit(1)
+}
+
+func loadConf(path string) (string, string, error) {
+	f, err := os.OpenFile(path, os.O_RDONLY, 0600)
+	if err != nil {
+		return "", "", err
+	}
+	defer f.Close()
+
+	sc := bufio.NewScanner(f)
+	if !sc.Scan() {
+		if err := sc.Err(); err != nil {
+			return "", "", err
+		}
+		return "", "", fmt.Errorf("cant read line")
+	}
+	row := sc.Text()
+
+	creds := strings.SplitN(row, ":", 2)
+	if len(creds) < 2 {
+		return "", "", fmt.Errorf("cant load credential")
+	}
+	return creds[0], creds[1], nil
+}
+
+func init() {
+	var c_path string
+	flag.StringVar(&c_path, "c", "", "path of credential")
+	flag.Parse()
+
+	if flag.NArg() < 3 {
+		die("Usage : onamaeddns <host> <domain> <ip address>")
+	}
+
+	host := flag.Arg(0)
+	if host == "" {
+		die("empty hostname")
+	}
+	domain := flag.Arg(1)
+	if domain == "" {
+		die("empty domain")
+	}
+	ipaddr := flag.Arg(2)
+	if ipaddr == "" {
+		die("empty ip address")
+	}
+
+	if c_path == "" {
+		usr, err := user.Current()
+		if err != nil {
+			die("%s", err)
+		}
+		c_path = usr.HomeDir + "/.onamaeddns"
+	}
+	user, pass, err := loadConf(c_path)
+	if err != nil {
+		die("%s", err)
+	}
+
+	User = user
+	Pass = pass
+	Domain = domain
+	Host = host
+	IpAddress = ipaddr
+}
+
+func main() {
+	if err := ddns(); err != nil {
+		die("%s", err)
+	}
 }
